ef202b2d1f03029b7b363b231aba6073084fcdc7
Andres Ruiz <andrsruiz@gmail.com>
2019-07-24 17:06:20 -0500

    Refactoring
    r/dailyprogrammer Challenge 1 [Easy] and [Intermediate]:
    - Move some code to functions.
    - Better algorithm for guess the number, the range is now dynamic.

--- subreddit-dailyprogrammer/challenge-1-easy.cpp
+++ subreddit-dailyprogrammer/challenge-1-easy.cpp
@@ -9,28 +9,40 @@
 
 #include <iostream>
 #include <fstream>
+using namespace std;
+
+// Function declaration
+int writeFile(string name, int age, string redditUsername);
 
 int main(){
- 	std::string name = "";
+ 	string name = "";
 	int age = 0;
-	std::string redditUsername = "";
-	std::ofstream myFile;
-	std::string myFileName = "reddit-user-info.txt";
+	string redditUsername = "";
 
-	std::cout << "Enter your name : ";
-	std::cin >> name;
-	std::cout << "Enter your age : ";
-	std::cin >> age;
-	std::cout << "Enter your reddit username : ";
-	std::cin >> redditUsername;
-	std::cout << std::endl;
+	// User input
+	cout << "Enter your name : ";
+	cin >> name;
+	cout << "Enter your age : ";
+	cin >> age;
+	cout << "Enter your reddit username : ";
+	cin >> redditUsername;
+	cout << std::endl;
 
-	myFile.open(myFileName);
-	std::cout << "Your name is " << name << ", you are " << age << " years old, and your username is " << redditUsername << "." << std::endl;
-	std::cout << "Wrinting file ... " << std::endl;
-	myFile << "Your name is " << name << ", you are " << age << " years old, and your username is " << redditUsername << ".";
-	std::cout << "Done." << std::endl;
-	myFile.close();
+	// Write file
+	writeFile(name, age, redditUsername);
 	
+	return 0;
+}
+
+int writeFile(string name, int age, string redditUsername){
+	ofstream myFile;
+	string myFileName = "reddit-user-info.txt";
+	
+	myFile.open(myFileName);
+	cout << "Wrinting file ... " << endl;
+	myFile << "Your name is " << name << ", you are " << age << " years old, and your username is " << redditUsername << ".";
+	myFile.close();
+	cout << "Done!" << endl;
+
 	return 0;
 }
--- subreddit-dailyprogrammer/challenge-1-intermediate.cpp
+++ subreddit-dailyprogrammer/challenge-1-intermediate.cpp
@@ -10,44 +10,59 @@
 #include <random>
 using namespace std;
 
-int const RANGE_FROM = 1;
-int const RANGE_TO = 100;
+// Function declaration
+void welcomeUser();
+int getRandNumber(int rangeMin, int rangeMax);
 
 int main(){
 	int userNumber;
 	int option;
 	int secretNumber;
-	int newSecretNumber;
+	int rangeMin = 1;
+	int rangeMax = 100;
 	
-	cout << "Welcome to 'GUESS THE NUMBER'\nI'm going to guess a number that you are going to think of." << endl;
-	cout << "The are going to be three cases : " << endl;
-	cout << "1. Too high 2. Too low 3. That is my number" << endl;
-	cout << "\nPress enter to play!.";
-	cin.ignore();
-
-	secretNumber = rand() % RANGE_TO + RANGE_FROM;
+	welcomeUser();
+	secretNumber = getRandNumber(rangeMin, rangeMax);
 	
 	do{
-		cout << "Are you thinking in the number : " << secretNumber << "?" << endl;
-		cout << "1. Too high 2. Too low 3. That is my number" << endl;
+		cout << "Are you thinking in the number : " << secretNumber << "?\n";
+		cout << "1. Too high 2. Too low 3. That is my number\n";
 		cout << "> ";
 		cin >> option;
 
 		switch(option){
-			case 1:
-				newSecretNumber = (RANGE_FROM + (rand() % secretNumber - RANGE_FROM + 1));
-				secretNumber = newSecretNumber;
+			case 1: // Number is too high
+				rangeMax = secretNumber;
+				secretNumber = getRandNumber(rangeMin, secretNumber);
 				break;
-			case 2:
-				newSecretNumber = (secretNumber + (rand() % RANGE_TO - secretNumber + 1));
-				secretNumber = newSecretNumber;
+			case 2: // Number is too low
+				rangeMin = secretNumber;
+				secretNumber = getRandNumber(secretNumber, rangeMax);
 				break;
 			case 3:
 				userNumber = secretNumber;
-				cout << "Nice!\n";
+				cout << "Nice! I guessed your number.\n";
+				break;
+			default:
+				cout << "Enter a valid option!\n";
 				break;
 		}
 	}while(userNumber != secretNumber);
-
+	
 	return 0;
+}
+
+void welcomeUser(){
+	cout << "Welcome to 'GUESS THE NUMBER'\nI'm going to guess a number that you are going to think of.\n";
+	cout << "The are going to be three cases : \n";
+	cout << "1. Too high 2. Too low 3. That is my number\n";
+	cout << "\nPress enter to play!.";
+	cin.ignore();
+}
+
+int getRandNumber(int rangeMin, int rangeMax){
+	random_device rd; // Random number from hardware
+	mt19937 eng(rd()); // Seed the generator
+	uniform_int_distribution<> distr(rangeMin, rangeMax); // Define the range
+	return distr(eng);
 }
