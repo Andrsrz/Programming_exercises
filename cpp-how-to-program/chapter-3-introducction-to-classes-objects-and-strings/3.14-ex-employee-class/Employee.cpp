046812a948456145bf5926eb7c67149e076995c2
Andres Ruiz <andrsruiz@gmail.com>
2019-08-19 16:27:45 -0500

    Add Employee class implementation

---
+++ cpp-how-to-program/chapter-3-introducction-to-classes-objects-and-strings/3.14-ex-employee-class/Employee.cpp
@@ -0,0 +1,41 @@
+/*
+ * C++ How To Program - 3.14 : Exercise Employee Class.
+*/
+
+#include <string>
+#include "Employee.h"
+
+// Constructor
+Employee::Employee(std::string eFirstName, std::string eLastName, int eMonthlySalary){
+	setFirstName(eFirstName);
+	setLastName(eLastName);
+	if(eMonthlySalary < 0){
+		setMonthlySalary(0);
+	}else if(eMonthlySalary >= 0){
+		setMonthlySalary(eMonthlySalary);
+	}
+}
+
+void Employee::setFirstName(std::string eFirstName){
+	firstName = eFirstName;
+}
+
+std::string Employee:getFirstName(){
+	return firstName;
+}
+
+void Employee::setLastName(std::string eLastName){
+	lastName = eLastName;
+}
+
+std::string Employee::getLastName(){
+	return lastName;
+}
+
+void Employee::setMonthlySalary(int eMonthlySalary){
+	monthlySalary = eMonthlySalary;
+}
+
+int Employee::getMonthlySalary(){
+	return monthlySalary;
+}
