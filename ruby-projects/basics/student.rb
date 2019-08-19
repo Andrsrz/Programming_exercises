905d72f76ef5efa8164c75bdd53d896ee9bd07b4
Andres Ruiz <andrsruiz@gmail.com>
2019-08-19 01:21:33 -0500

    Modify Student class

--- ruby-projects/basics/student.rb
+++ ruby-projects/basics/student.rb
@@ -3,7 +3,19 @@
 class Student
 	attr_accessor :first_name, :last_name, :primary_phone_number
 
-	def introduction
-		puts "Hi, I'm #{first_name}!"
+	def introduction(target)
+		puts "Hi #{target}, I'm #{first_name}!"
+	end
+
+	def favorite_number
+		7 # return value
 	end
 end
+
+# New Student instance
+andres = Student.new
+# Setting it's attribute
+andres.first_name = "Andres"
+# Using methods
+andres.introduction('Frida')
+puts "Andres's favorite number is #{andres.favorite_number}."
