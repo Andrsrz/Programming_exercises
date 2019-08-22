605ffd5dea7b712339ef57981a30e437f4720a81
Andres Ruiz <andrsruiz@gmail.com>
2019-08-22 17:22:55 -0500

    Add HeartRate class interface

---
+++ cpp-how-to-program/chapter-3-introducction-to-classes-objects-and-strings/3.16-mad-target-heart-rate-calculator/HeartRate.h
@@ -0,0 +1,27 @@
+/*
+ * C++ How To Program - 3.16 : Making a Difference Target Heart Rate Calculator
+*/
+
+class HeartRate{
+public:
+	HeartRate(std::string, std::string, int, int, int); // Constructor
+	void setFirstName(std::string); // Setters and getters
+	std::string getFirstName();
+	void setLastName(std::string);
+	std::string getLastName();
+	void setBirthMonth(int);
+	int getBirthMonth();
+	void setBirthDay(int);
+	int getBirthDay();
+	void setBirthYear(int);
+	int getBirthYear();
+	int getAge(); // Calculate the age
+	int getMaximumHeartRate(int); // Calculate maximum heart rate
+	int getTargetHeartRate(int); // Calculate target heart rate
+private:
+	std::string firstName;
+	std::string lastName;
+	int birthMonth;
+	int birthDay;
+	int birthYear; // Attributes
+};
