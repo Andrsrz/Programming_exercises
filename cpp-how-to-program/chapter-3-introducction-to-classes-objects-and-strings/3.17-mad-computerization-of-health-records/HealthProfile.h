0fa6bf56252103dcafec736f9e8941d9ecec9cda
Andres Ruiz <andrsruiz@gmail.com>
2019-08-28 18:02:46 -0500

    Add HealthProfile class interface

---
+++ cpp-how-to-program/chapter-3-introducction-to-classes-objects-and-strings/3.17-mad-computerization-of-health-records/HealthProfile.h
@@ -0,0 +1,41 @@
+/*
+ * C++ How to Programm - 3.17 : Making a Difference Health Profile 
+*/
+
+class HealthProfile{
+public:
+	// Constructor
+	HealthProfile(std::string, std::string, std::string, int, int, int, float, float);
+	// Setters and getters
+	void setFirstName(std::string);
+	std::string getFirstName();
+	void setLastName(std::string);
+	std::string getLastName();
+	void setGender(std::string);
+	std::string getGender();
+	void setBirthMonth(int);
+	int getBirthMonth();
+	void setBirthDay(int);
+	int getBirthDay();
+	void setBirthYear(int);
+	int getBirthYear();
+	void setHeight(float);
+	float getHeight();
+	void setWeight(float);
+	float getWeight();
+	// Methods
+	int getAge();
+	float getMaximumHeartRate(int);
+	float getTargetHeartRate(float);
+	float getBodyMassIndex();
+privte:
+	// Attributes
+	std::string firstName;
+	std::string lastName;
+	std::string gender;
+	int birthMonth;
+	int birthDay;
+	int birthYear;
+	float height;
+	float weight;
+};
