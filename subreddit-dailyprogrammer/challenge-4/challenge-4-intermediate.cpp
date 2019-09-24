ea05f0d56ced767f875b5a9a171c5b83f79cfd55
Andres Ruiz <andrsruiz@gmail.com>
2019-09-24 17:19:46 -0500

    Add Addition Function
    It has errors in the convertion of char to int.
    As it is now, only works from 2 up to 9 for every digit
    and only in operation with two digits.

--- subreddit-dailyprogrammer/challenge-4/challenge-4-intermediate.cpp
+++ subreddit-dailyprogrammer/challenge-4/challenge-4-intermediate.cpp
@@ -3,32 +3,78 @@
  * Description in txt file
 */
 
-/*
-TODO :
-	parse through input (string)
-	make operations
-	return answer
-*/
-
 #include <iostream>
 #include <string>
+
+// Avoid magic numbers
+const int TWO = 2;
+const int THREE = 3;
+const char PLUS = '+';
+
+// Function declaration
+int makeOperation(std::string);
+int makeAddition(int, int);
+int makeSubtraction(int, int);
+int makeDivision(int, int);
+int makeMultiplication(int, int);
 
 int main(){
 	bool menu = true;
 
+	std::cout << "\tWelcome to Terminal Calculator!\n";
 	while(menu){
+		// MENU
 		std::string option = "";
-		std::cout << "\tWelcome to Terminal Calculator!\n"
-			<< "Enter any operation ( If you want to quit type : quit )"
+		std::cout << "Enter any operation ( If you want to quit type : quit )"
 			<< "\n > ";
 		std::getline(std::cin, option);
 
 		if(option == "quit"){
+			// EXIT
 			menu = false;
 		}else{
-			// Do operation
+			if(option.size() >= THREE){
+				std::cout << " < " << makeOperation(option) << std::endl;
+			}else{
+				std::cout << "\tPlease enter a valid operation!\n" << std::endl;
+			}
 		}
 	}
 
+	return 0;
+}
+
+// Function implementation
+int makeOperation(std::string operation){
+	int result = 0;
+
+	for(int i = 0; i < operation.size(); i++){
+		// Get only the operation character
+		if(i % TWO != 0){
+			if(operation[i] == PLUS){
+				result += makeAddition((int)operation[i--] - '0', (int)operation[++i] - '0');
+			}
+		}
+	}
+
+	return result;
+}
+
+int makeAddition(int first, int second){
+	int addition = 0;
+	std::cout << first << " " << second << "\t";
+	addition = first + second;
+	return addition;
+}
+
+int makeSubtraction(int first, int second){
+	return 0;
+}
+
+int makeDivision(int first, int second){
+	return 0;
+}
+
+int makeMultiplication(int first, int second){
 	return 0;
 }
