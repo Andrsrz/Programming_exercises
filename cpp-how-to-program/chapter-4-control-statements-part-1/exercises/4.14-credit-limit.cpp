a798b2837696387b843deb81700a31f70519a441
Andres Ruiz <andrsruiz@gmail.com>
2019-09-26 16:09:37 -0500

    Add Customer class interface

--- cpp-how-to-program/chapter-4-control-statements-part-1/exercises/4.14-credit-limit.cpp
+++ cpp-how-to-program/chapter-4-control-statements-part-1/exercises/4.14-credit-limit.cpp
@@ -5,6 +5,30 @@
 
 #include <iostream>
 
+// Customer class
+class Customer{
+public:
+	Customer(int, int, int, int, int); // Constructor
+	void setAccountNumber(int); // Setters and getters
+	int getAccountNumber();
+	void setStartBalance(int);
+	int getStartBalance();
+	void setTotalCharges(int);
+	int getTotalCharges();
+	void setTotalCredit(int);
+	int getTotalCredit();
+	void setAllowedCredit(int);
+	int getAllowedCredit();
+	std::string calculateNewBalance(); // Methods
+private:
+	// Attributes
+	int accountNumber;
+	int startBalance;
+	int totalCharges;
+	int totalCredit;
+	int allowedCredit;
+};
+
 int main(){
 	return 0;
 }
