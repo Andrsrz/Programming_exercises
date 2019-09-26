4626999bd2c0687fd65e4e1dd6b8ee65acb7cc9a
Andres Ruiz <andrsruiz@gmail.com>
2019-09-26 16:42:03 -0500

    Add Customer class implementation

--- cpp-how-to-program/chapter-4-control-statements-part-1/exercises/4.14-credit-limit.cpp
+++ cpp-how-to-program/chapter-4-control-statements-part-1/exercises/4.14-credit-limit.cpp
@@ -8,18 +8,76 @@
 // Customer class
 class Customer{
 public:
-	Customer(int, int, int, int, int); // Constructor
-	void setAccountNumber(int); // Setters and getters
-	int getAccountNumber();
-	void setStartBalance(int);
-	int getStartBalance();
-	void setTotalCharges(int);
-	int getTotalCharges();
-	void setTotalCredit(int);
-	int getTotalCredit();
-	void setAllowedCredit(int);
-	int getAllowedCredit();
-	std::string calculateNewBalance(); // Methods
+	// Constructor
+	Customer(int cAccountNumber, int cStartBalance,
+		int cTotalCharges, int cTotalCredit, int cAllowedCredit){
+		setAccountNumber(cAccountNumber);
+		setStartBalance(cStartBalance);
+		setTotalCharges(cTotalCharges);
+		setTotalCredit(cTotalCredit);
+		setAllowedCredit(cAllowedCredit);
+	}
+
+	// Setters and getters
+	void setAccountNumber(int cAccountNumber){
+		accountNumber = cAccountNumber;
+	}
+
+	int getAccountNumber(){
+		return accountNumber;
+	}
+
+	void setStartBalance(int cStartBalance){
+		startBalance = cStartBalance;
+	}
+
+	int getStartBalance(){
+		return startBalance;
+	}
+
+	void setTotalCharges(int cTotalCharges){
+		totalCharges = cTotalCharges;
+	}
+
+	int getTotalCharges(){
+		return totalCharges;
+	}
+
+	void setTotalCredit(int cTotalCredit){
+		totalCredit = cTotalCredit;
+	}
+
+	int getTotalCredit(){
+		return totalCredit;
+	}
+
+	void setAllowedCredit(int cAllowedCredit){
+		allowedCredit = cAllowedCredit;
+	}
+
+	int getAllowedCredit(){
+		return allowedCredit;
+	}
+
+	// Methods
+	std::string calculateNewBalance(){
+		std::string a = "";
+		int newBalance = 0;
+
+		// New balance formula
+		newBalance = startBalance + totalCharges - totalCredit;
+
+		if(newBalance > allowedCredit){
+			a = "Account Number : " + std::to_string(accountNumber)
+				+ "\nCredit Limit : " + std::to_string(allowedCredit)
+				+ "\nNew Balance : " + std::to_string(newBalance)
+				+ "\nCredit Limit Exceeded";
+		}else{
+			a = "All good!";
+		}
+
+		return a;
+	}
 private:
 	// Attributes
 	int accountNumber;
