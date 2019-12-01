df917ac9c448b48eaa8a9ae89e0c5665c870d74c
Andres Ruiz <andrsruiz@gmail.com>
2019-12-01 14:46:13 -0600

    Add Exercise 4.33
    Control statements and math.

---
+++ cpp-how-to-program/chapter-4-control-statements-part-1/exercises/4.33-right-triangle-sides.cpp
@@ -0,0 +1,27 @@
+/*
+  * C++ How to Program - 4.33 Sides of a Right Triangle.
+  * Description in txt file.
+*/
+
+#include <iostream>
+
+int main(){
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	
+	// Check user input
+	while( ( a == 0 ) && ( b == 0 ) && ( c == 0 ) ){
+		std::cout << "Enter three nonzero values : ";
+		std::cin >> a >> b >> c;	
+	}
+	
+	// Pythagoras' Theorem
+	if( ( c * c ) == ( ( a * a ) + ( b * b ) ) ){
+		std::cout << "Yes. This is a right triangle!." << std::endl;
+	}else{
+		std::cout << "No. This is not a right triangle!." << std::endl;
+	}
+
+	return 0;
+}
