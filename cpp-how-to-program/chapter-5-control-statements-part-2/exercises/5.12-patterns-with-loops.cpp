fe05210e473316cbea8ed6417bd31040019fbfed
Andres Ruiz <andrsruiz@gmail.com>
2019-12-13 16:41:58 -0600

    Update Exercise 5.12
    Point C

--- cpp-how-to-program/chapter-5-control-statements-part-2/exercises/5.12-patterns-with-loops.cpp
+++ cpp-how-to-program/chapter-5-control-statements-part-2/exercises/5.12-patterns-with-loops.cpp
@@ -7,8 +7,10 @@
 
 const char ASTERISK = '*';
 const char SPACE = ' ';
+const char RETURN = '\r';
 const char NEW_LINE = '\n';
 // Avoid magic numbers
+const int TWO = 2;
 const int TEN = 10;
 
 int main(){
@@ -24,6 +26,17 @@ int main(){
 	std::cout << "B :" << std::endl;
 	for(int i = 1; i <= TEN; i++){
 		for(int j = TEN; j >= i; j--){
+			std::cout << ASTERISK;
+		}
+		std::cout << NEW_LINE;
+	}
+	// C
+	std::cout << "C :" << std::endl;
+	for(int row = 1; row <= TEN; row++){
+		for(int spaces = TWO; spaces <= row; spaces++){
+			std::cout << SPACE;
+		}
+		for(int asterisks = TEN; asterisks >= row; asterisks--){
 			std::cout << ASTERISK;
 		}
 		std::cout << NEW_LINE;
