7c1e139cd9d5af0d8596d73bf787969b98b847da
Andres Ruiz <andrsruiz@gmail.com>
2020-04-17 13:14:15 -0500

    Change to tabs

--- cpp-how-to-program/chapter-6-functions-and-recursion/exercises/6.21-even.cpp
+++ cpp-how-to-program/chapter-6-functions-and-recursion/exercises/6.21-even.cpp
@@ -11,17 +11,17 @@ const int TWO = 2;
 bool isEven(int);
 
 int main(){
-    std::cout << "1 : " << isEven(1) << "\n";
-    std::cout << "1 : " << isEven(1) << "\n";
-    std::cout << "2 : " << isEven(2) << "\n";
-    std::cout << "3 : " << isEven(3) << "\n";
-    std::cout << "5 : " << isEven(5) << "\n";
-    std::cout << "8 : " << isEven(8) << "\n";
-    std::cout << "13 : " << isEven(13) << "\n";
-    return 0;
+	std::cout << "1 : " << isEven(1) << "\n";
+	std::cout << "1 : " << isEven(1) << "\n";
+	std::cout << "2 : " << isEven(2) << "\n";
+	std::cout << "3 : " << isEven(3) << "\n";
+	std::cout << "5 : " << isEven(5) << "\n";
+	std::cout << "8 : " << isEven(8) << "\n";
+	std::cout << "13 : " << isEven(13) << "\n";
+	return 0;
 }
 
 // Function implementation
 bool isEven(int x){
-    return (x % TWO) == 0 ? true : false;
+	return (x % TWO) == 0 ? true : false;
 }
