154961c6bb1c6ae04cb48e552a23b2953684fd8b
Andres Ruiz <andrsruiz@gmail.com>
2020-04-26 11:01:11 -0500

    Add Exercise 6.30 Test
    The test doesn't passed. Still working on it.

---
+++ cpp-how-to-program/ch6/tests/6-30/test-printReversedNumber.cpp
@@ -0,0 +1,22 @@
+#include <catch2/catch.hpp>
+#include <string>
+#include <vector>
+
+std::vector<int> six {6};
+std::vector<int> twenty_eigth {2,8};
+std::vector<int> three {8,1,2,8};
+std::vector<int> four {4,9,6};
+
+std::string printReversedNumber(std::vector<int> digits){
+	std::string str = "";
+	for(int i = digits.size() - 1; i > 0; i--)
+		str += std::to_string(digits[i]);
+	return str;
+}
+
+TEST_CASE("1: printReversedNumber", "[multi-file:2]"){
+	REQUIRE(printReversedNumber(six) == "6");
+	REQUIRE(printReversedNumber(twenty_eigth) == "82");
+	REQUIRE(printReversedNumber(three) == "8218");
+	REQUIRE(printReversedNumber(four) == "694");
+}
