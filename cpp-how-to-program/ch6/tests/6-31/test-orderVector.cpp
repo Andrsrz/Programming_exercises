5a5e4559552a64a43ebac212e59afd185bfab734
Andres Ruiz <andrsruiz@gmail.com>
2020-05-07 16:57:19 -0500

    Update 6.31 Unit test

---
+++ cpp-how-to-program/ch6/tests/6-31/test-orderVector.cpp
@@ -0,0 +1,22 @@
+#include <catch2/catch.hpp>
+
+/* Insertion sort */
+std::vector<int> orderVector(std::vector<int> vect){
+	for(int i = 1; i < static_cast<int>(vect.size()); i++){
+		int j = i;
+		while(j > 0 && (vect[j - 1] > vect[j])){
+			int temp = vect[j];
+			vect.at(j) = vect[j - 1];
+			vect.at(j - 1) = temp;
+			j--;
+		}
+	}
+
+	return vect;
+}
+
+TEST_CASE("1: orderVector", "[multi-file:3]"){
+	REQUIRE_THAT(orderVector({ 5 }), Catch::Equals<int>({ 5 }));
+	REQUIRE_THAT(orderVector({ 2, 6, 1, 3 }), Catch::Equals<int>({ 1, 2, 3, 6 }));
+	REQUIRE_THAT(orderVector({ 100, 99, 98, 97 }), Catch::Equals<int>({ 97, 98, 99, 100 }));
+}
